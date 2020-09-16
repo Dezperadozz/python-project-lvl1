f8fe0f3cb3f0301a6139a7ec7e5e2237cc4cbfc0
Dezperadozz <sxoma01@gmail.com>
2020-09-16 22:46:35 +0300

    third step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,15 @@
+import prompt
+
 def greet():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
